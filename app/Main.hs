9357436f180b054a4d6a55a850f79c51f4adc1ed
Hanabi-TheFox <pintoethan@cy-tech.fr>
2024-02-26 12:57:14 +0100

    Updated main

--- app/Main.hs
+++ app/Main.hs
@@ -2,9 +2,12 @@ module Main (main) where
 
 import Statistic.Huffman
 import LZ.LZW
+import LZ.LZ78
 
 main :: IO ()
 main = do
+  -- Méthode Huffman
+  putStrLn "Methods : Huffman\n"
   let input = "test bola CHIPOLATA 42! [ü]"
       (encodingTree, compressedBits) = compressHuffman input
       decompressedResult = decompressHuffman encodingTree compressedBits
@@ -14,12 +17,30 @@ main = do
   putStrLn $ "Decompressed Result: " ++ show decompressedResult
 
   -- Méthode LZW
+  putStrLn "\n\nMethods : LZW\n"
   let dataToCompress = "trololo"
-  let compressedData = compress dataToCompress
-  let decompressedData = uncompress compressedData
+  let compressedData = LZ.LZW.compress dataToCompress
+  let decompressedData = LZ.LZW.uncompress compressedData
 
   putStrLn $ "Data originale: " ++ dataToCompress
   putStrLn $ "Données compressées: " ++ show compressedData
   case decompressedData of
     Just str -> putStrLn $ "Données décompressées: " ++ str
     Nothing -> putStrLn "Erreur lors de la décompression."
+
+   -- test for the LZ78
+  let inputString = "belle echelle !"
+      compressed = LZ.LZ78.compress inputString
+      decompressed = LZ.LZ78.uncompress compressed
+
+  putStrLn "\n\nMethods : LZ78\n"
+  putStrLn "Input String:"
+  putStrLn inputString
+
+  putStrLn "\nCompressed Codes:"
+  print compressed
+
+  putStrLn "\nDecompressed Result:"
+  case decompressed of
+    Just result -> putStrLn result
+    Nothing -> putStrLn "Unable to decompress the input"
