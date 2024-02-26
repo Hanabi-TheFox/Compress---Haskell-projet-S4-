95ed42ae6576f051159bcbb1d438a5ed61038cec
Hanabi-TheFox <pintoethan@cy-tech.fr>
2024-02-26 12:54:13 +0100

    Implementation of LZ78 compress and uncompress methods

--- src/LZ/LZ78.hs
+++ src/LZ/LZ78.hs
@@ -1,15 +1,39 @@
 {- |
   Module      : LZ.LZ78
   Description : An implementation of LZ78 method
-  Maintainer  : ???
+  Maintainer  : Ethan Pinto (pintoethan@cy-tech.fr)
 -}
 module LZ.LZ78(compress, uncompress) where
 
+import Data.Map (Map)
+import qualified Data.Map as Map
+
 -- | LZ78 compress method
 compress :: String -> [(Int, Char)]
-compress _ = undefined -- TODO
+compress = compress' Map.empty 0
+  where
+    compress' :: Map String Int -> Int -> String -> [(Int, Char)]
+    compress' _ _ [] = []
+    compress' dict nextIndex input =
+      let (prefix, suffix) = findLongestPrefix dict input
+          dict' = Map.insert (prefix ++ [head suffix]) (nextIndex + 1) dict
+      in (Map.findWithDefault 0 prefix dict, head suffix) : compress' dict' (nextIndex + 1) (tail suffix)
+
+    findLongestPrefix :: Map String Int -> String -> (String, String)
+    findLongestPrefix dict input = go [] input
+      where
+        go prefix [] = (prefix, [])
+        go prefix (x:xs) =
+          case Map.lookup (prefix ++ [x]) dict of
+            Just _ -> go (prefix ++ [x]) xs
+            Nothing -> (prefix, x:xs)
 
 -- | LZ78 uncompress method
 -- If input cannot be uncompressed, returns `Nothing`
 uncompress :: [(Int, Char)] -> Maybe String
-uncompress _ = undefined -- TODO
+uncompress = Just . uncompress' []
+  where
+    uncompress' :: [String] -> [(Int, Char)] -> String
+    uncompress' _ [] = []
+    uncompress' dict ((0, c) : xs) = c : uncompress' (dict ++ [[c]]) xs
+    uncompress' dict ((n, c) : xs) = (dict !! (n - 1) ++ [c]) ++ uncompress' (dict ++ [dict !! (n - 1) ++ [c]]) xs
